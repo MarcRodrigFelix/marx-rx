6357dae634ce8a964d877fc1b4048c7bba5ca941
Marcos <m.rodrig.felix@gmail.com>
2022-01-23 15:49:27 -0800

    add create action in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,18 @@ class SessionsController < ApplicationController
 
   def create
 byebug
-    user = User.find_by(username: params[:username])
+    user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
+
+    if user
+      session[:user_id] = user.id
+      render json: {
+        status :created,
+        logged_in: true,
+        user: user
+      }
+    else
+      render json: { status: 401 }
+    end
   end
 
 end
